fa43f71318e317a0a4cf5992dd5a123a0bf78e53
Hanafi Abdullah <hanafikonten@gmail.com>
2023-03-03 21:10:55 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+const app = express();
+
+// //middleware
+// app.use((req, res, next)=>{
+//     console.log("ada request masuk");
+//     next();
+// })
+
+//route
+app.get("/",(_req,res)=>{
+    res.send("Hanafi");
+});
+
+app.listen(3000,()=>{
+    console.log("Server sedang berjalan!");
+});
